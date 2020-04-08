a7c60f1c286d9ff636fa96a95a49f418f4c3556d
Naveen <spk2naveen@hotmail.com>
2020-04-09 03:26:26 +0530

    Update ProductFunction.cs

--- src/MongoFunction/ProductFunction.cs
+++ src/MongoFunction/ProductFunction.cs
@@ -26,7 +26,7 @@ namespace MongoFunction
             }
 
             // Hard-coded Connection String & Database name, as they are not the point of focus for this exercise
-            var connectionString = "mongodb+srv://app:helloWorld18@cluster0-p9r6y.mongodb.net/test?retryWrites=true&w=majority";
+            var connectionString = "<Replace with your connection string>";
             var databaseName = "GroceryStore";
 
             var repository = new ProductsRepository(
